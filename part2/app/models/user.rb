26c88718f5d2c31fd9048be2fdb23ca526e7b2d9
Pat Shaughnessy <pat@patshaughnessy.net>
2009-05-16 18:35:07 -0400

    ?style=:style

--- part2/app/models/user.rb
+++ part2/app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_attached_file :avatar,
                     :styles => { :thumb => "75x75>", :small => "150x150>" },
                     :path => ':rails_root/non-public/system/:attachment/:id/:style/:basename.:extension',
-                    :url => '/:class/:id/:attachment'
+                    :url => '/:class/:id/:attachment?style=:style'
 
 end
