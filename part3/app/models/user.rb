3d41e460ce7bc3f250d6595da66dec09db801b65
Pat Shaughnessy <pat@patshaughnessy.net>
2009-05-30 08:20:15 -0400

    default scope for user model

--- part3/app/models/user.rb
+++ part3/app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
                     :storage => :database,
                     :styles => { :thumb => "75x75>", :small => "150x150>" },
                     :url => '/:class/:id/:attachment?style=:style'
-
+  default_scope select_without_file_columns_for(:avatar)
 end
